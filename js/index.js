624842d1f48d4628b17908901eb7d7709f9e22a8
AlexeiKhomitsa <92182450+AlexeiKhomitsa@users.noreply.github.com>
2022-04-17 14:10:23 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,29 +6,19 @@ function checkTest() {
 	let result = 0;
 
 	const a1 = document.getElementById('q1').value;
-	if (a1 === '4') {
-		result++;
-	}
+	if (a1 === '4') result++;
 
 	const a2 = document.getElementById('q2').value;
-	if (a2 === '6') {
-		result++;
-	}
+	if (a2 === '6') result++;
 
 	const a3 = document.getElementById('q3').value;
-	if (a3 === '10') {
-		result++;
-	}
+	if (a3 === '10') result++;
 
 	const a4 = document.getElementById('q4').value;
-	if (a4 === '-1') {
-		result++;
-	}
+	if (a4 === '-1') result++;
 
 	const a5 = document.getElementById('q5').value;
-	if (a5 === '1') {
-		result++;
-	}
+	if (a5 === '1') result++;
 
 	alert('Правильных ответов: ' + result);
 }
